1b77de5cd6c9d97b46a43a60871e40083cb4214c
Ema <emanuele.alberti@yahoo.it>
2018-09-08 12:37:01 +0200

    First commit of my whole life

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-print("Hello world, this is my new project I'm so excited!")
+value = {"I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000}
 
-ciao = "New string"
+def romanToInt(s):
+    number = 0
+    i = 0
+    numLen = len(s)
+    while i < numLen:
+        if i < numLen-1 and value[s[i]] < value[s[i+1]]:
+            number += value[s[i+1]] - value[s[i]]
+            i += 2
+        else:
+            number += value[s[i]]
+            i += 1
+    return number
+
+# main
+while 1:
+    romanNumber = input("Roman number: ")
+    if romanNumber == "stop":
+        break
+    print("Integer number: %d" % romanToInt(romanNumber))
+
+
+
+
+
+
+
 
